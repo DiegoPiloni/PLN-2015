7bbaad246e61eb99f1f3f1e95accd5248b40c43d
Diego Piloni <diegorpiloni@gmail.com>
2015-10-02 18:26:34 -0300

    Ejercicio 1, con readme, falta ambiguedad

---
+++ README.md
@@ -0,0 +1,24 @@
+Ejercicio 1
+-----------
+
+### Estadísticas:
+
+Sents: 17379
+Tokens: 517300
+Words: 46483
+Tags: 48
+
+#### 10 most frequent tags:
+
+|Tag | Freq  |   %   | Descripción  | 5 most frequent words                                            |
+|----|-------|-------|--------------|------------------------------------------------------------------|
+|nc  | 92002 | 17.79 | Nombre Común          | 'años', 'presidente', 'millones', 'equipo', 'partido' |
+sp  | 79904 | 15.45 | Preposición           | 'de', 'en', 'a', 'del', 'con'                          |
+da  | 54552 | 10.55 | Articulo              | 'la', 'el', 'los', 'las', 'El'                         |
+vm  | 50609 |  9.78 | Verbo principal       | 'está', 'tiene', 'dijo', 'puede', 'hace'               |
+aq  | 33904 |  6.55 | Adjetivo calificativo | 'pasado', 'gran', 'mayor', 'nuevo', 'próximo'          |
+fc  | 30148 |  5.83 | Puntuación            | ','                                                    |
+np  | 29113 |  5.63 | Nombre Propio         | 'Gobierno', 'España', 'PP', 'Barcelona', 'Madrid'      |
+fp  | 21157 |  4.09 | Puntuación            | '.', '(', ')'                                          |
+rg  | 15333 |  2.96 | Adverbio General      | 'más', 'hoy', 'también', 'ayer', 'ya'                  |
+cc  | 15023 |  2.90 | Conjunción Coordinada | 'y', 'pero', 'o', 'Pero', 'e'                          |
--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -8,16 +8,59 @@ Options:
   -h --help     Show this screen.
 """
 from docopt import docopt
-
+from collections import defaultdict
 from corpus.ancora import SimpleAncoraCorpusReader
 
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
-    # load the data
+    # Load the data
     corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/')
     sents = corpus.tagged_sents()
 
-    # compute the statistics
-    print('sents: {}'.format(len(sents)))
+    # Compute the statistics
+
+    # Sents
+    print('Sents: {}'.format(len(sents)))
+
+    # Words and Tags
+    words = []
+
+    # Counted tags
+    counted_tags = defaultdict(int)
+
+    # Counted words for each tag
+    counted_tag_words = defaultdict(lambda: defaultdict(int))
+
+    for sent in sents:
+        for w, t in sent:
+            words.append(w)
+            counted_tags[t] += 1
+            counted_tag_words[t][w] += 1
+
+    word_ocurrences = len(words)
+    vocab_w = set(words)
+
+    counted_tags = list(counted_tags.items())
+    counted_tags.sort(key=lambda x: x[1], reverse=True)
+
+    # Total num of tags
+    total_tags = sum([x[1] for x in counted_tags])
+
+    print('Tokens: {}'.format(word_ocurrences))
+    print('Words: {}'.format(len(vocab_w)))
+    print('Tags: {}'.format(len(counted_tags)))
+
+    # Most frequent tags
+    print("\n10 most frequent tags:\n")
+    print("Tag | Freq  |   %   | 5 most frequent words")
+    print("-------------------------------------------")
+    for i in range(10):
+        tag = counted_tags[i][0]
+        freq = counted_tags[i][1]
+        perc = freq / total_tags * 100
+        lw = list(counted_tag_words[tag].items()) # List of words with tag
+        lw.sort(key = lambda x: x[1], reverse=True)
+        mfw = [x[0] for x in lw[:5]]  # 5 most freq words with tag
+        print(tag + "  | {0} | {1:>5.2f} | {2}".format(freq, perc, mfw))
