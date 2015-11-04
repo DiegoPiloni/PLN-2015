792f92ff23c6deb912dcd33bb83c66693ba5eee7
Diego Piloni <diegorpiloni@gmail.com>
2015-11-04 19:50:46 -0300

    Test for ambig sent

--- parsing/tests/test_cky_parser.py
+++ parsing/tests/test_cky_parser.py
@@ -64,17 +64,10 @@ class TestCKYParser(TestCase):
             (5, 5): {'Adj': Tree.fromstring("(Adj crudo)")},
 
             (1, 2): {'NP': Tree.fromstring("(NP (Det el) (Noun gato))")},
-            #(2, 3): {},
-            #(3, 4): {},
             (4, 5): {'NP': Tree.fromstring("(NP (Noun pescado) (Adj crudo))")},
 
-            #(1, 3): {},
-            #(2, 4): {},
             (3, 5): {'VP': Tree.fromstring(
                 "(VP (Verb come) (NP (Noun pescado) (Adj crudo)))")},
-
-            #(1, 4): {},
-            #(2, 5): {},
 
             (1, 5): {'S': Tree.fromstring(
                 """(S
@@ -85,95 +78,94 @@ class TestCKYParser(TestCase):
         }
         self.assertEqual(parser._bp, bp)
 
-
     def test_parse_ambig(self):
         grammar = PCFG.fromstring(
             """
-                S -> NP VP              [1.0]
-                VP -> Vt NP             [0.8]
-                VP -> VP PP             [0.2]
+                VP -> Vt NP             [0.75]
+                VP -> VP PP             [0.25]
                 NP -> DT NN             [0.8]
                 NP -> NP PP             [0.2]
                 PP -> IN NP             [1.0]
                 Vt -> 'saw'             [1.0]
-                NN -> 'man'             [0.1]
-                NN -> 'woman'           [0.1]
-                NN -> 'telescope'       [0.3]
-                NN -> 'dog'             [0.5]
+                NN -> 'telescope'       [0.2]
+                NN -> 'dog'             [0.8]
                 DT -> 'the'             [1.0]
-                IN -> 'with'            [0.6]
-                IN -> 'in'              [0.4]
+                IN -> 'with'            [1.0]
             """)
 
         parser = CKYParser(grammar)
 
-        lp, t = parser.parse('the man saw the dog with the telescope'.split())
+        lp, t = parser.parse('saw the dog with the telescope'.split())
 
         # check chart
         pi = {
-            (1, 1): {'DT': log2(1.0)},
-            (2, 2): {'NN': log2(0.1)},
-            (3, 3): {'Vt': log2(1.0)},
-            (4, 4): {'DT': log2(1.0)},
-            (5, 5): {'NN': log2(0.5)},
-            (6, 6): {'IN': log2(0.6)},
-            (7, 7): {'DT': log2(1.0)},
-            (8, 8): {'NN': log2(0.3)},
+            (1, 1): {'Vt': log2(1.0)},
+            (2, 2): {'DT': log2(1.0)},
+            (3, 3): {'NN': log2(0.8)},
+            (4, 4): {'IN': log2(1.0)},
+            (5, 5): {'DT': log2(1.0)},
+            (6, 6): {'NN': log2(0.2)},
 
-            (1, 2): {'NP': log2(0.8 * 1.0 * 0.1)},
-            (2, 3): {},
+            (1, 2): {},
+            (2, 3): {'NP': log2(0.8 * 1.0 * 0.8)},
             (3, 4): {},
-            (4, 5): {'NP': log2(0.8 * 1.0 * 0.5)},
-            (5, 6): {},
-            (6, 7): {},
-            (7, 8): {'NP': log2(0.8 * 1.0 * 0.3)},
+            (4, 5): {},
+            (5, 6): {'NP': log2(0.8 * 1.0 * 0.2)},
 
-            (1, 3): {},
+            (1, 3): {'VP': log2(0.75) + log2(1.0) + log2(0.8 * 1.0 * 0.8)},
             (2, 4): {},
-            (3, 5): {'VP': log2(0.8) + log2(1.0) + log2(0.8 * 1.0 * 0.5)},
-            (4, 6): {},
-            (5, 7): {},
-            (6, 8): {'PP': log2(1.0) + log2(0.6) + log2(0.8 * 1.0 * 0.3)},
+            (3, 5): {},
+            (4, 6): {'PP': log2(1.0) + log2(1.0) + log2(0.8 * 1.0 * 0.2)},
 
             (1, 4): {},
             (2, 5): {},
             (3, 6): {},
-            (4, 7): {},
-            (5, 8): {},
 
-            (1, 5): {'S': 
-                     log2(1.0) +  # S -> NP VP
-                     log2(0.8 * 1.0 * 0.1) +  # left part
-                     log2(0.8) + log2(1.0) + log2(0.8 * 1.0 * 0.5)},  # right part
-            (2, 6): {},
-            (3, 7): {},
-            (4, 8): {'NP': 
-                     log2(0.2) +  # NP -> NP PP
-                     log2(0.8 * 1.0 * 0.5) +  # left part
-                     log2(1.0) + log2(0.6) + log2(0.8 * 1.0 * 0.3)},  # right part
+            (1, 5): {},
+            (2, 6): {'NP':
+                     log2(0.2) +  # rule NP -> NP PP
+                     log2(0.8 * 1.0 * 0.8) +  # left part
+                     log2(1.0) + log2(1.0) + log2(0.8 * 1.0 * 0.2)},  # right part
 
-            (1, 6): {},
-            (2, 7): {},
-            (3, 8): {'VP':
-                     log2(0.2) +  # rule VP -> VP PP
-                     log2(0.8) + log2(1.0) + log2(0.8 * 1.0 * 0.5) +  # left part
-                     log2(1.0) + log2(0.6) + log2(0.8 * 1.0 * 0.3)},  # right part
+            (1, 6): {'VP':
+                     log2(0.25) +  # rule VP -> VP PP
+                     log2(0.75) + log2(1.0) + log2(0.8 * 1.0 * 0.8) +  # left part
+                     log2(1.0) + log2(1.0) + log2(0.8 * 1.0 * 0.2)  # right part
+                     },
 
-            (1, 7): {},
-            (2, 8): {},
-
-            (1, 8): {'S':
-                     log2(1.0) +  # rule S -> NP VP
-                     log2(0.8 * 1.0 * 0.1) +  # left part
-                     log2(0.2) +
-                     log2(0.8) + log2(1.0) + log2(0.8 * 1.0 * 0.5) +
-                     log2(1.0) + log2(0.6) + log2(0.8 * 1.0 * 0.3)},  # right part
         }
         self.assertEqualPi(parser._pi, pi)
 
-
         # check partial results
+        bp = {
+            (1, 1): {'Vt': Tree.fromstring("(Vt saw)")},
+            (2, 2): {'DT': Tree.fromstring("(DT the)")},
+            (3, 3): {'NN': Tree.fromstring("(NN dog)")},
+            (4, 4): {'IN': Tree.fromstring("(IN with)")},
+            (5, 5): {'DT': Tree.fromstring("(DT the)")},
+            (6, 6): {'NN': Tree.fromstring("(NN telescope)")},
 
+            (2, 3): {'NP': Tree.fromstring("(NP (DT the) (NN dog))")},
+            (5, 6): {'NP': Tree.fromstring("(NP (DT the) (NN telescope))")},
+
+            (1, 3): {'VP': Tree.fromstring("(VP (Vt saw) (NP (DT the) (NN dog)))")},
+            (4, 6): {'PP': Tree.fromstring("(PP (IN with) (NP (DT the) (NN telescope)))")},
+
+            (2, 6): {'NP': Tree.fromstring(
+                """(NP
+                    (NP (DT the) (NN dog))
+                    (PP (IN with) (NP (DT the) (NN telescope)))
+                   )
+                """)},
+
+            (1, 6): {'VP': Tree.fromstring(
+                """(VP
+                    (VP (Vt saw) (NP (DT the) (NN dog)))
+                    (PP (IN with) (NP (DT the) (NN telescope)))
+                   )
+                """)},
+        }
+        self.assertEqual(parser._bp, bp)
 
     def assertEqualPi(self, pi1, pi2):
         self.assertEqual(set(pi1.keys()), set(pi2.keys()))
@@ -185,4 +177,3 @@ class TestCKYParser(TestCase):
                 prob1 = d1[k2]
                 prob2 = d2[k2]
                 self.assertAlmostEqual(prob1, prob2)
-
